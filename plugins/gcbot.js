3d3b120d5e6574b9f207c5f12f83b65423b23100
Zaki1726 <100398414+Zaki1726@users.noreply.github.com>
2022-08-18 10:15:32 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -3,7 +3,7 @@
 let handler = async m => m.reply(`
 ╭═════════════════
 ║   𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋
-║https://chat.whatsapp.com/Jzd9DEVBODtNBk1VCNrV
+║https://chat.whatsapp.com/J6278UOXBAaCXUOnp2ehua
 ╰══════════════════
 
 `.trim()) 
